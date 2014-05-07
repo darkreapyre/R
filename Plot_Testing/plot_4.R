cd09becf26f9785aa4dbd8cbd5c8e9fa2a6ebdcd
darkreapyre <darkreapyre@gmail.com>
2014-05-07 13:54:59 -0700

    formatting changes

--- Plot_Testing/plot_4.R
+++ Plot_Testing/plot_4.R
@@ -31,7 +31,7 @@ plot(date, df$Sub_metering_1, type = "l", ylab = "Energy Submetering")
 lines(date, df$Sub_metering_2, type = "l", col = "red")
 lines(date, df$Sub_metering_3, type = "l", col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty=1, lwd =2.5, col = c("black", "red", "blue"), bty = "n")
+       lty = 1, lwd = 2.5, col = c("black", "red", "blue"), bty = "n")
 plot(date, df$Global_reactive_power, type = "l", xlab = "datetime",
      ylab = "Global_reactive_power")
 dev.off()
